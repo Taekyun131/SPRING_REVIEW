1f02eb9521779ef0b3f2811c37b3c0de1e760fa0
Taekyun131 <stk131@naver.com>
2025-04-24 18:00:48 +0900

    20250424_servletConfig

---
+++ src/main/java/sec01/ex01/FirstServlet.java
@@ -0,0 +1,34 @@
+package sec01.ex01;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// HttpServlet을 상속받고 3개의 생명주기 메서드를 구현하는 서블릿 작성
+// web.xml파일에서 서블릿 매핑 or 애너테이션을 사용해 매핑
+// 서블릿은 메모리에 존재하는 서블릿을 재사용함으로써 훨씬 빠르고 효율적으로 동작
+public class FirstServlet extends HttpServlet {
+
+	@Override
+	public void init() throws ServletException {
+		System.out.println("init 메서드 호출");
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("doGet 메서드 호출");
+	}
+
+	@Override
+	public void destroy() {
+		System.out.println("destroy 메서드 호출");
+	}
+	
+	
+	
+		
+	
+}
